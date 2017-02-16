1a14e376435eaa272bcf1af65dd3f8fc2de669c2
A-sunny-ren <1398488165@qq.com>
2017-02-16 13:57:14 +0800

    vue饿了么实践

---
+++ vue/src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">header</div>
+</template>
+
+<script type="text/ecmascript-6">
+    export {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
