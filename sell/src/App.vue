1a8bbef0bc5dbb547928e5dd940088ab0e8dcd24
A-sunny-ren <1398488165@qq.com>
2017-10-15 22:12:31 +0800

    购物车

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -8,7 +8,7 @@
     </div>
     <!-- 路由出口 -->
   	<!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
+    <router-view :seller="seller"></router-view>
   </div>
 </template>
 
@@ -28,7 +28,6 @@
 				response = response.body
 				if (response.errno === ERR_OK) {
 					this.seller = response.data
-					console.log(this.seller)
 				}
 			})
 		},
@@ -41,7 +40,7 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "common/stylus/mixin.styl"
 
-  .tab
+.tab
     display: flex
     width: 100%
     height: 40px
@@ -49,12 +48,12 @@
     // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
     border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
-      flex: 1
-      text-align: center
-      & > a
-        display: block
-        font-size: 14px
-        color: rgb(77, 85, 93)
-        &.router-link-active
-          color: rgb(240, 20, 20)
+        flex: 1
+        text-align: center
+        & > a
+            display: block
+            font-size: 14px
+            color: rgb(77, 85, 93)
+            &.router-link-active
+                color: rgb(240, 20, 20)
 </style>
